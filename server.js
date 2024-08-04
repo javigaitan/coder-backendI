e81e36296f1a06dbf27dc487d8b76617ca193ecc
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2024-08-04 18:21:12 -0300

    add(server.js)Creacion del servidor

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Felicitaciones! Pusiste en marcha el portal ${PORT}`);
+});
